287130bb6452b71215cf8b6671dd7f476789fd21
Jo Brunner <jo.brunner@mayflower.de>
2017-06-02 17:35:56 +0200

    Simple user interaction

--- RolfOffl/MainViewController.swift
+++ RolfOffl/MainViewController.swift
@@ -8,35 +8,41 @@ import CoreLocation
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var fireButton: UIButton!
+    
+    @IBOutlet weak var plzLabel: UILabel!
+
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
-        let location = CLLocation(latitude:49.783180555556,
-                                  longitude:9.9371583333333)
-        
-        if let plz = RegionService().service(request: Postleitzahl(location)) {
-            print("PLZ ist: \(plz)")
-        }
-        else {
-            print("PLZ konnte nicht ermittelt werden.")
-        }
     }
     
     override func didReceiveMemoryWarning() {
         
         super.didReceiveMemoryWarning()
     }
+}
+
+// MARK: - IBActions
+
+extension MainViewController {
+
+    @IBAction func fireButtonTouch(_ sender: UIButton) {
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+        plzLabel.text = "Bestimme deine Position"
+
+        // trigger Location Service with callback:
+        
+        let location = CLLocation(latitude:49.783180555556,
+                                  longitude:9.9371583333333)
+        
+        if let plz = RegionService().service(request: Postleitzahl(location)) {
+            
+            plzLabel.text = plz
+        }
+        else {
+            plzLabel.text = "PLZ nicht gefunden"
+        }
+    }
+
 }
