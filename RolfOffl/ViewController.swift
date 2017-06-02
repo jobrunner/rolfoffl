56f99ee24d914246b57a3778fac85056e75f004a
Jo Brunner <jo.brunner@mayflower.de>
2017-06-02 14:55:53 +0200

    Inital commit of Rolf Offline aka RolfOffl

---
+++ RolfOffl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RolfOffl
+//
+//  Created by Jo Brunner on 02.06.17.
+//  Copyright © 2017 Jo Brunner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
