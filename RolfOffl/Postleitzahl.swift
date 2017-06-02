ff8612edad40c014fb3bd4ae8bf8d6050e6566ff
Jo Brunner <jo.brunner@mayflower.de>
2017-06-02 16:34:15 +0200

    Adds Postleitzahlen Configuration

---
+++ RolfOffl/Postleitzahl.swift
@@ -0,0 +1,24 @@
+//
+//  Created by Jo Brunner on 02.06.17.
+//  Copyright © 2017 Jo Brunner. All rights reserved.
+//
+
+import CoreLocation
+
+class Postleitzahl: RegionRequest {
+    
+    let query: String
+    let params: [BindParam]
+    
+    init(_ location: CLLocation) {
+
+        query = "SELECT (plz || \" \" || note) as value FROM plz WHERE ST_Contains(Geometry, ST_Point(?,?))";
+
+        params = [
+            BindParam(type: kBindingTypeDouble,
+                      value: location.coordinate.longitude),
+            BindParam(type: kBindingTypeDouble,
+                      value: location.coordinate.latitude)
+        ]
+    }
+}
