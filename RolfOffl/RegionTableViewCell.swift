a728e527df416a716bf198851ede27ce26f6984b
Jo Brunner <jo.brunner@mayflower.de>
2017-10-01 17:50:21 +0200

    Adds a city cell view

---
+++ RolfOffl/RegionTableViewCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class RegionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ortLabel: UILabel!
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+    }
+}
