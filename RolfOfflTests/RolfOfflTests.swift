3b0d394a861c8f9ed132abafefec600ae04be537
Jo Brunner <jo.brunner@mayflower.de>
2017-10-02 10:52:41 +0200

    Adds a base integration test against zip code gis database

--- RolfOfflTests/RolfOfflTests.swift
+++ RolfOfflTests/RolfOfflTests.swift
@@ -1,35 +1,81 @@
-//
-//  RolfOfflTests.swift
-//  RolfOfflTests
-//
-//  Created by Jo Brunner on 02.06.17.
-//  Copyright © 2017 Jo Brunner. All rights reserved.
-//
-
 import XCTest
+import CoreLocation
 @testable import RolfOffl
 
 class RolfOfflTests: XCTestCase {
     
+    // Hausen im Landkreis Neuwied, Germany
+    let testLocation = CLLocation(latitude: CLLocationDegrees(50.551948),
+                                  longitude: CLLocationDegrees(7.385075))
+
     override func setUp() {
+
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testStandardIntegration() {
+
+        guard let items = RegionService().service(request: Postleitzahl(testLocation)) else {
+            return XCTAssert(false, "Integration test on standard location is brocken")
+        }
+
+        XCTAssertTrue(items.count > 0)
+        XCTAssertEqual(items.count, 8);
+        print(items)
+        let shouldLand = "Deutschland"
+        let shouldBundesland = "Rheinland-Pfalz"
+        let shouldLandkreis = "Landkreis Neuwied"
+        let orte: Set<String> = [
+            "Bad Hönningen",
+            "Breitscheid",
+            "Dattenberg",
+            "Hausen (Wied)",
+            "Hümmerich",
+            "Kasbach-Ohlenberg",
+            "Leubsdorf",
+            "Roßbach"
+        ]
+        
+        items.forEach { result in
+            if let land = result["land"] {
+                XCTAssertEqual(land, shouldLand)
+            }
+            else {
+                XCTAssert(false, "land is not properly set")
+            }
+
+            if let bundesland = result["bundesland"] {
+                XCTAssertEqual(bundesland, shouldBundesland)
+            }
+            else {
+                XCTAssert(false, "bundesland is not properly set")
+            }
+
+            if let landkreis = result["landkreis"] {
+                XCTAssertEqual(landkreis, shouldLandkreis)
+            }
+            else {
+                XCTAssert(false, "landkreis is not properly set")
+            }
+
+            if let ort = result["ort"] {
+                XCTAssert(orte.contains(ort!), "False ort")
+            }
+            else {
+                XCTAssert(false, "Ort is not properly set")
+            }
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testPerformanceStandard() {
+
         self.measure {
-            // Put the code you want to measure the time of here.
+            _ = RegionService().service(request: Postleitzahl(testLocation))
         }
     }
     
